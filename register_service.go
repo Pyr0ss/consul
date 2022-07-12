5a8f62dcae611a963a2478f7880ea5b40196040d
Pyr0ss <pguillon67@gmail.com>
2022-07-12 11:47:49 +0200

    added go script that registers a service with a health check included

---
+++ register_service.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/hashicorp/consul/api"
+)
+
+func main() {
+	// Get a new client
+	client, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		panic(err)
+	}
+
+	//Get agent
+	agent := client.Agent()
+	
+	//Creat check
+	check := &api.AgentServiceCheck{
+		Args: []string{"go", "run", "google_health_check.go"},
+		Interval: "5s",
+        Timeout: "1s",
+	}
+
+	//Register service
+	reg := &api.AgentServiceRegistration{
+		Name: "google",
+		Port: 80,
+		Check: check,
+	}
+	if err := agent.ServiceRegister(reg); err != nil {
+		panic(err)
+	}
+}
